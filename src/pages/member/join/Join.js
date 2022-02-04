cb05dd0353ba3ea618ac05c3ab8fe10566d9c6f8
박준석 <vawoojs@naver.com>
2022-02-04 17:41:35 +0900

    First Commit

--- src/pages/member/join/Join.js
+++ src/pages/member/join/Join.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Join = () => {
+function Join() {
+    const [inputs, setInputs] = useState({
+        action : 'insert'
+        memId : '',
+        memPw : '',
+        memPwChk : '',
+        nickName : ''
+    })
+
+    const { memId, memPw, memPwChk, nickName } = inputs;
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+
+        setInputs({
+            ...inputs,
+            [name] : value
+        });
+    };
+
+
+
+    const onValidator = (e) => {
+
+        setInputs({
+            ...inputs,
+            action : "insert"
+        });
+
+        if(inputs.memId != null) {
+
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form action={inputs.action}>
                 <div>
                     <span>
                         <label htmlFor={"memId"}> ID : </label>
                     </span>
                     <span>
-                        <input type={"text"} id={"memId"} name={"memId"}/>
+                        <input type={"text"} id={"memId"} name={"memId"} onChange={onChange} value={memId} />
                     </span>
                 </div>
                 <div>
@@ -17,7 +50,7 @@ const Join = () => {
                         <label htmlFor={"memPw"}> PW : </label>
                     </span>
                     <span>
-                        <input type={"password"} id={"memPw"} name={"memPw"} autoComplete={"off"}/>
+                        <input type={"password"} id={"memPw"} name={"memPw"} autoComplete={"off"} onChange={onChange} value={memPw} />
                     </span>
                 </div>
                 <div>
@@ -25,7 +58,7 @@ const Join = () => {
                         <label htmlFor={"memPwChk"}> PW RE : </label>
                     </span>
                     <span>
-                        <input type={"password"} id={"memPwChk"} autoComplete={"off"}/>
+                        <input type={"password"} id={"memPwChk"} name={"memPwChk"} autoComplete={"off"} onChange={onChange} value={memPwChk} />
                     </span>
                 </div>
                 <div>
@@ -33,12 +66,12 @@ const Join = () => {
                         <label htmlFor={"nickName"}> NickName : </label>
                     </span>
                     <span>
-                        <input type={"text"} id={"nickName"} name={"nickName"}/>
+                        <input type={"text"} id={"nickName"} name={"nickName"} onChange={onChange} value={nickName} />
                     </span>
                 </div>
                 <div>
                     <span>
-                        <button type={"submit"}>subbbb</button>
+                        <button type={"submit"} onClick={onValidator}>subbbb</button>
                     </span>
                     <span>
                         <button type={"reset"}>reset</button>
